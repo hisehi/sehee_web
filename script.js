9958247231eb3f014e9ed971f2e56d2d46bc0f19
hisehi <163225952+hisehi@users.noreply.github.com>
2024-11-08 23:51:14 +0900

    1차

---
+++ script.js
@@ -0,0 +1,66 @@
+
+$(function() {
+  var data = [
+    {
+      action: 'type',
+      strings: ["2024... Korea Polytechnics..."],
+      output: '<span class="gray">Seoul Jungsu Campus...</><br>&nbsp;',
+      
+      postDelay: 1000
+    },
+  { 
+    action: 'type',
+    strings: ["Produced by... Hwang Sehee"],
+    output: 'Graduation_work...<br><br>',
+    postDelay: 1000
+  },
+  { 
+    action: 'type',
+    strings: ["Please just for a minute...", 'Press any key to Start'],
+    postDelay: 2000
+  }
+  
+];
+  runScripts(data, 0);
+});
+
+function runScripts(data, pos) {
+    var prompt = $('.prompt'),
+        script = data[pos];
+    if(script.clear === true) {
+      $('.history').html(''); 
+    }
+    switch(script.action) {
+        case 'type':
+          // cleanup for next execution
+          prompt.removeData();
+          $('.typed-cursor').text('');
+          prompt.typed({
+            strings: script.strings,
+            typeSpeed: 30,
+            callback: function() {
+              var history = $('.history').html();
+              history = history ? [history] : [];
+              history.push('	&#60;div&#62; ' + prompt.text());
+              if(script.output) {
+                history.push(script.output);
+                prompt.html('');
+                $('.history').html(history.join('<br>'));
+              }
+              // scroll to bottom of screen
+              $('section.terminal').scrollTop($('section.terminal').height());
+              // Run next script
+              pos++;
+              if(pos < data.length) {
+                setTimeout(function() {
+                  runScripts(data, pos);
+                }, script.postDelay || 1000);
+              }
+            }
+          });
+          break;
+        case 'view':
+
+          break;
+    }
+}
